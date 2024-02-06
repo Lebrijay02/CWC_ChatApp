703ab6783a1636c74d262ac61bdaa88a0aafdfbd
Lebrijay02 <juan.lebrija2002@gmail.com>
2024-02-05 19:24:37 -0600

    Initial Commit

---
+++ CWC_ChatApp/CWC_ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CWC_ChatAppApp.swift
+//  CWC_ChatApp
+//
+//  Created by Juan Lebrija on 05/02/24.
+//
+
+import SwiftUI
+
+@main
+struct CWC_ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CWC_ChatApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CWC_ChatApp
+//
+//  Created by Juan Lebrija on 05/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
